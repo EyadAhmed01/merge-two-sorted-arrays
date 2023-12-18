52dea843e229c91d6e274d3d6151c64db46965ab
EyadAhmed01 <103265469+EyadAhmed01@users.noreply.github.com>
2023-12-18 02:28:44 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i;
+    printf("enter size of the array");
+    scanf("%d",&n);
+int a[n];
+for(i=0;i<n;i++)
+{
+    printf("enter a[%d];",i);
+    scanf("%d",&a[i]);
+
+}
+}
